c72697eacf5939f5aadb86b60d26476e122b29ec
Maikol <maikolsoro.z1998@gmail.com>
2023-06-30 14:18:45 -0700

    Fisnished Design IMC

---
+++ app/src/main/java/com/example/androidmaster/imccalculator/ResultIMCActivity.kt
@@ -0,0 +1,16 @@
+package com.example.androidmaster.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmaster.R
+import com.example.androidmaster.imccalculator.ImcCalculatorActivity.Companion.IMC_KEY
+
+class ResultIMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imcactivity)
+
+        val result:Double = intent.extras?.getDouble(IMC_KEY) ?: -1.0
+
+    }
+}
