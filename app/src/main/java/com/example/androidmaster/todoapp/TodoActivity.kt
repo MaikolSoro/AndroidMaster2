927622a685d556f25c563817e208408db7b542d5
Maikol <maikolsoro.z1998@gmail.com>
2023-06-30 18:55:30 -0700

    Add Design Tasks, RecylerView

--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -2,22 +2,33 @@ package com.example.androidmaster.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
+import com.example.androidmaster.todoapp.TaskCategory.*
 import com.example.androidmaster.todoapp.adapter.CategoriesAdapter
+import com.example.androidmaster.todoapp.adapter.TasksAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
 
     private val categories = listOf(
-        TaskCategory.Business,
-        TaskCategory.Personal,
-        TaskCategory.Other
+        Business,
+        Personal,
+        Other
+    )
+    private val tasks = mutableListOf(
+        Task("Oracle", Business, true),
+        Task("Progammer", Personal, true),
+        Task("SQLOther", Other, true)
     )
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
 
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var taskAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
@@ -28,13 +39,18 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
-
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        taskAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = taskAdapter
     }
 
 }
---
+++ app/src/main/java/com/example/androidmaster/todoapp/adapter/TasksAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.androidmaster.todoapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidmaster.R
+import com.example.androidmaster.todoapp.Task
+import com.example.androidmaster.todoapp.TasksViewHolder
+
+class TasksAdapter(var tasks: List<Task>) :
+    RecyclerView.Adapter<TasksViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        return TasksViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+      holder.render(tasks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+}
