b7e7b04f9eb6788ff1bab6d724cb08104aaade7e
Maikol <maikolsoro.z1998@gmail.com>
2023-06-30 20:24:01 -0700

    Add Dialog and Tasks

--- app/src/main/java/com/example/androidmaster/todoapp/adapter/TasksAdapter.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/adapter/TasksAdapter.kt
@@ -7,15 +7,17 @@ import com.example.androidmaster.R
 import com.example.androidmaster.todoapp.Task
 import com.example.androidmaster.todoapp.TasksViewHolder
 
-class TasksAdapter(var tasks: List<Task>) :
+class TasksAdapter(var tasks: List<Task>, private val onTaskSelected: (Int) -> Unit) :
     RecyclerView.Adapter<TasksViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
         return TasksViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
-      holder.render(tasks[position])
+        holder.render(tasks[position])
+        holder.itemView.setOnClickListener { onTaskSelected(position) }
     }
 
     override fun getItemCount(): Int {
