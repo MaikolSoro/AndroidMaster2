f00d1f284a92e8aa245f2b01a249b1e8caf09910
Maikol <maikolsoro.z1998@gmail.com>
2023-06-30 17:32:41 -0700

    Add RecyclerView and Design Category

--- app/src/main/java/com/example/androidmaster/MainActivity.kt
+++ app/src/main/java/com/example/androidmaster/MainActivity.kt
@@ -6,19 +6,37 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.androidmaster.firstapp.FirstAppActivity
 import com.example.androidmaster.imccalculator.ImcCalculatorActivity
+import com.example.androidmaster.todoapp.TodoActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnGreetApp:Button
+    private lateinit var btnImcApp:Button
+    private lateinit var btnTODO:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnGreetApp = findViewById<Button>(R.id.btnHelloApp)
-        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        btnGreetApp = findViewById(R.id.btnHelloApp)
+        btnImcApp = findViewById(R.id.btnIMCApp)
+        btnTODO = findViewById(R.id.btnTODO)
 
+        initListeners()
+
+    }
+
+    private fun initListeners() {
         btnGreetApp.setOnClickListener { navigateGreetApp()  }
 
         btnImcApp.setOnClickListener { navigateToIcmApp() }
+
+        btnTODO.setOnClickListener { navigateToTodoApp() }
     }
+
+    private fun navigateToTodoApp() {
+        val intent = Intent(this, TodoActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun navigateGreetApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
